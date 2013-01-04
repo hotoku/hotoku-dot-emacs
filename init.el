ca8445ed049e65931ce0aca382418a80bb4e57aa
hotoku <hotoku66@gmail.com>
2013-01-05 06:47:40 +0900

    Adding twittering-mode.

--- init.el
+++ init.el
@@ -412,10 +412,41 @@ the next ARG files are used.  Just \\[universal-argument] means the current file
 ;;; haskell-mode
 (load "haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+
+;;; twittering-mode (via http://fukuyama.co/twittering-mode)
+;; twittering-mode読み込み
+(require 'twittering-mode)
+;; 起動時パスワード認証 *要 gpgコマンド
+(setq twittering-use-master-password t)
+;; パスワード暗号ファイル保存先変更 (デフォはホームディレクトリ)
+(setq twittering-private-info-file "~/.emacs.d/twittering-mode.gpg")
+;; 表示する書式 区切り線いれたら見やすい
+(setq twittering-status-format "%i @%s %S
+%T
+%p [%@]%r %R %f%L")
+;; アイコンを表示する
+(setq twittering-icon-mode t)
+;; アイコンサイズを変更する *48以外を希望する場合 要 imagemagickコマンド
+(setq twittering-convert-fix-size 24)
+;; 更新の頻度（秒）
+(setq twittering-timer-interval 40)
+;; ツイート取得数
+(setq twittering-number-of-tweets-on-retrieval 50)
+;; o で次のURLをブラウザでオープン
+(add-hook 'twittering-mode-hook
+          (lambda ()
+            (local-set-key (kbd "o")
+               (lambda ()
+                 (interactive)
+                 (twittering-goto-next-uri)
+                 (execute-kbd-macro (kbd "C-m"))
+                 ))))
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
