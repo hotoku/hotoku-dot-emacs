d5484d1e6aee0af601a5efe5b8014883e48588e0
hotoku <hotoku66@gmail.com>
2013-03-04 20:48:44 +0900

    Disabling truncate lines when file extension is "runit".

--- config/80_ess.el
+++ config/80_ess.el
@@ -1,6 +1,8 @@
 (require 'ess-site)
 
 
+
+
 (require 'align)
 (add-to-list 'align-rules-list
              '(ess-assignment-operator
@@ -9,3 +11,14 @@
                (modes  . '(ess-mode))))
 (add-to-list 'auto-mode-alist
              '("\\.runit" . R-mode))
+
+
+
+
+(add-hook 'ess-mode-hook
+	  (lambda ()
+	    (let ((ext (replace-regexp-in-string
+			".*\\.\\([^\\.]+\\)$" "\\1"
+			buffer-file-name)))
+	      (when (string= ext "runit")
+		(setq truncate-lines t)))))
