b2c2c56a4d05ebbd3605e6ff2c86952707ecfe60
hotoku <hotoku66@gmail.com>
2013-03-17 00:07:45 +0900

    adding auto-async-byte-compile.

--- init.el
+++ init.el
@@ -224,6 +224,14 @@
 
 
 
+;;; auto-async-byte-compile
+(require 'auto-async-byte-compile)
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+
+
+
+
 ;;; anything
 (defvar org-directory "")
 (require 'anything-startup)
