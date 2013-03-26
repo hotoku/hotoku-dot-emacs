2ec100933a0da39e1175ac24eb8985012226a4db
hotoku <hotoku66@gmail.com>
2013-03-26 22:54:41 +0900

    move twitter private info.

--- config/80_twitter.el
+++ config/80_twitter.el
@@ -2,7 +2,7 @@
 ;; 起動時パスワード認証 *要 gpgコマンド
 (setq twittering-use-master-password t)
 ;; パスワード暗号ファイル保存先変更 (デフォはホームディレクトリ)
-(setq twittering-private-info-file "~/.emacs.d/twittering-mode.gpg")
+(setq twittering-private-info-file "~/.emacs.d/secret/twittering-mode.gpg")
 ;; 表示する書式 区切り線いれたら見やすい
 (setq twittering-status-format "%i @%s %S
 %T
