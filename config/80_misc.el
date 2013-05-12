1dbab78318e5216fbd00b956a14cc0f963bb48d2
hotoku <hotoku66@gmail.com>
2013-05-12 15:39:31 +0900

    remove anything related function.

--- config/80_misc.el
+++ config/80_misc.el
@@ -44,37 +44,6 @@ to be searched by howm."
 
 
 
-;;; anything git
-(require 'cl)
-(require 'anything)
-(defun anything-c-sources-git-project-for (pwd)
-  (loop for elt in
-        '(("Modified files (%s)" . "--modified")
-          ("Untracked files (%s)" . "--others --exclude-standard")
-          ("All controlled files in this project (%s)" . ""))
-        collect
-        `((name . ,(format (car elt) pwd))
-          (init . (lambda ()
-                    (unless (and ,(string= (cdr elt) "") ;update candidate buffer every time except for that of all project files
-                                 (anything-candidate-buffer))
-                      (with-current-buffer
-                          (anything-candidate-buffer 'global)
-                        (insert
-                         (shell-command-to-string
-                          ,(format "git ls-files $(git rev-parse --show-cdup) %s"
-                                   (cdr elt))))))))
-          (candidates-in-buffer)
-          (type . file))))
-(defun anything-git-project ()
-  (interactive)
-  (let* ((pwd default-directory)
-         (sources (anything-c-sources-git-project-for pwd)))
-    (anything-other-buffer sources
-                           (format "*Anything git project in %s*" pwd))))
-
-
-
-
 ;;; gomi file
 (defun gomi-file ()
   (interactive)
