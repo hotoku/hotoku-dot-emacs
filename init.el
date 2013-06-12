edd6762a448f34a545afa29174385928b33f17b6
hotoku <hotoku66@gmail.com>
2013-06-12 20:09:14 +0900

    adding run-test-target on safe-local-variable-values.

--- init.el
+++ init.el
@@ -290,3 +290,9 @@
 ;;; helm
 (require 'helm-config)
 (helm-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((run-test-target . test/test-ofx\.py)))))
