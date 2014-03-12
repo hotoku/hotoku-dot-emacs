c6a7d952cb102a7c13de189fba013502bdcb824d
Yasunori Horikoshi <horikoshi@localhost.localdomain>
2014-03-11 22:49:10 -0700

    remove multi-term

--- config/00_terminal.el
+++ config/00_terminal.el
@@ -1,9 +1,3 @@
 (require 'ucs-normalize)
 (setq file-name-coding-system 'utf-8-hfs)
 (setq locale-coding-system 'utf-8-hfs)
-
-
-(require 'multi-term)
-(setq multi-term-program shell-file-name)
-(setq term-unbind-key-list
-      (append '("C-f" "C-b") term-unbind-key-list))
