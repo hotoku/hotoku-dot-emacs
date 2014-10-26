5ea3bf15eefbbfeed32ccb408ec8d3cf037234b0
Yasunori Horikoshi <horikoshi.et.al@gmail.com>
2014-10-26 14:46:20 +0900

    disable flymake

--- config/80_flymake.el
+++ config/80_flymake.el
@@ -6,10 +6,6 @@
          (local-file  (file-relative-name
                        temp-file
                        (file-name-directory buffer-file-name))))
-    (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" "-std=c++11" local-file))))
+    (list "clang++" (list "-Wall" "-Wextra" "-fsyntax-only" "-std=c++11" local-file))))
 
 (push '("\\.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
-
-(add-hook 'c++-mode-hook
-          '(lambda ()
-             (flymake-mode t)))
