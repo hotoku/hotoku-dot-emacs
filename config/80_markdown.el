93fddbbe8bfbecbb6b8b2cce55fdc0f9034df0d4
Yasunori Horikoshi <y_horikoshi@r.recruit.co.jp>
2015-03-10 18:44:08 +0900

    config/80_markdown.el

---
+++ config/80_markdown.el
@@ -0,0 +1,5 @@
+(add-hook 'markdown-mode-hook
+          '(lambda ()
+             (set-face-foreground 'markdown-header-face-2 "purple4")
+             (set-face-foreground 'markdown-header-face-3 "DarkOrchid4")
+             (set-face-foreground 'markdown-header-face-4 "MediumOrchid4")))
