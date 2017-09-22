c473acabdc705888e35e1b0aa7bd7f26057aa0c5
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2017-09-22 23:49:42 +0900

    markdown: prefix item of unordered list

--- init.el
+++ init.el
@@ -311,6 +311,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(indent-tabs-mode nil)
+ '(markdown-unordered-list-item-prefix "* ")
  '(safe-local-variable-values
    (quote
     ((run-test-target . test\.sh)
