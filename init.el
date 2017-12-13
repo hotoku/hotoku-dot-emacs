e6e3002343ec3b6ab15e0fcf8c86b33b2c9cd0bd
Yasunori Horikoshi <y_horikoshi@r.recruit.co.jp>
2017-12-13 17:01:11 +0900

    js-indent-level: 4 -> 2

--- init.el
+++ init.el
@@ -309,6 +309,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(indent-tabs-mode nil)
+ '(js-indent-level 2)
  '(markdown-unordered-list-item-prefix "* ")
  '(python-shell-interpreter "python3")
  '(safe-local-variable-values
