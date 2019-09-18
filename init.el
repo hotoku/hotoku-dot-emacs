4713aa8c40c7cdd0e6a3e68d9605ca6d95122c5b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-09-18 11:22:20 +0900

    Helmの設定を色々追加
    元ネタはこれ。
    https://qiita.com/jabberwocky0139/items/86df1d3108e147c69e2c

--- init.el
+++ init.el
@@ -304,6 +304,17 @@
 (setq helm-delete-minibuffer-contents-from-point t)
 (setq helm-ff-auto-update-initial-value nil)
 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
+
 
 
 
