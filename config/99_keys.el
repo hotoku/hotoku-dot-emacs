d96749e5ead0bcb5ada7fc4dda0281615487c7bc
hotoku <horikoshi.et.al@gmail.com>
2019-11-02 11:21:54 +0900

    evernoteのキーバインドを捨てる

--- config/99_keys.el
+++ config/99_keys.el
@@ -7,13 +7,6 @@
 
 (global-set-key (kbd "C-x H") 'hatenahelper-mode)
 (global-set-key (kbd "C-c , ,") 'howm-menu)
-;; (global-set-key "\C-cec" 'evernote-create-note)
-;; (global-set-key "\C-ceo" 'evernote-open-note)
-;; (global-set-key "\C-ces" 'evernote-search-notes)
-;; (global-set-key "\C-ceS" 'evernote-do-saved-search)
-;; (global-set-key "\C-cew" 'evernote-write-note)
-;; (global-set-key "\C-cep" 'evernote-post-region)
-;; (global-set-key "\C-ceb" 'evernote-browser)
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-.") 'ace-window)
 (global-set-key (kbd "C-1") 'delete-other-windows)
