a82cf4ba9af54711f05d92b75460b4403f5491d6
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-12-10 09:29:50 +0900

    cssの編集

--- config/80_markdown.el
+++ config/80_markdown.el
@@ -3,3 +3,6 @@
 
 ;; htmlに仕込むCSS
 (setq markdown-css-paths (list (expand-file-name "~/.emacs.d/resource/github-markdown.css")))
+
+(setq markdown-preview-stylesheets
+      (list (expand-file-name "~/.emacs.d/resource/github-markdown.css")))
