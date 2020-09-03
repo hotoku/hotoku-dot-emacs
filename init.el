64b5cb841624637d5fa640b616b0c962e7b8df1e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-09-03 21:42:32 +0900

    C-x C-f

--- init.el
+++ init.el
@@ -180,7 +180,17 @@
 	(setq ns-command-modifier (quote meta))
 	(setq ns-alternate-modifier (quote super))
 	(global-set-key (kbd "C-;") 'helm-mini)
-	(global-set-key (kbd "C-.") 'other-window-or-split))
+	(global-set-key (kbd "C-.") 'other-window-or-split)
+
+	;; change default directory for C-x C-f
+	(when (version< "27.0" emacs-version)
+		(defun ad:helm-find-files (f prompt)
+			"When uploading to Emacs 27.1, default directory for buffers like *Emacs* changes from ~ to /.
+This is inconvinient when opening file at the beginning of Emacs session."
+			(when (equal default-directory "/")
+				(setq default-directory "~/"))
+			(funcall f prompt))
+		(advice-add 'helm-find-files :around #'ad:helm-find-files)))
 
 ;;; when terminal
 (when (not window-system)
