5af80bc28e04a8046531d1a21e45c35a989dc491
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-09-07 17:34:30 +0900

    color

--- init.el
+++ init.el
@@ -225,14 +225,23 @@
                (local-set-key "\C-\M-e" 'foiltex-next-page)
                (add-hook 'before-save-hook 'equally-spaced-make-gap-buffer :local t))))
 
+(yh/config "javascript"
+  (setq js-indent-level 2))
+
 (yh/config "when Emacs.app on Mac"
   (when (eq window-system 'ns)
+    ;; meta key
     (setq ns-command-modifier (quote meta))
     (setq ns-alternate-modifier (quote super))
+
+    ;; global key assign only for Emacs.app
     (global-set-key (kbd "C-;") 'helm-mini)
     (global-set-key (kbd "C-.") 'other-window-or-split)
+
+    ;; theme
     (require 'dakrone-theme)
     (load-theme 'dakrone)
+    (set-face-foreground 'font-lock-comment-face "IndianRed")
 
     ;; change default directory for C-x C-f
     (when (version< "27.0" emacs-version)
