0c97fc836130efb062628e2c2b2d84ed040f5073
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-09-17 08:55:24 +0900

    make indent wip

--- init.el
+++ init.el
@@ -144,20 +144,22 @@ the next ARG files are used.  Just \\[universal-argument] means the current file
   :config
   (defun yh/makefile-indent-line ()
     "https://emacs.stackexchange.com/questions/3074/customizing-indentation-in-makefile-mode"
-    (save-excursion
-      (forward-line 0)
-      (cond
-       ;; keep TABs
-       ((looking-at "\t")
-        t)
-       ;; indent continuation lines to 4
-       ((and (not (bobp))
-             (= (char-before (1- (point))) ?\\))
-        (delete-horizontal-space)
-        (indent-to 4))
-       ;; delete all other leading whitespace
-       ((looking-at "\\s-+")
-        (replace-match "")))))
+    (if (and (= (point-beginning-of-line) (point))
+             (string= (yh/current-line) ""))
+        (progn (insert "\t") (forward-char 1))
+      (save-excursion
+        (forward-line 0)
+        (cond
+         ;; keep TABs
+         ((looking-at "\t") t)
+         ;; indent continuation lines to 4
+         ((and (not (bobp))
+               (= (char-before (1- (point))) ?\\))
+          (delete-horizontal-space)
+          (indent-to 4))
+         ;; delete all other leading whitespace
+         ((looking-at "\\s-+")
+          (replace-match ""))))))
 
   (add-hook 'makefile-mode-hook
             (lambda ()
