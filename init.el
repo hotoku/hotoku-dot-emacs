d57f7e7a84ed7337607dec11f79d602c96b45a72
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-01-26 17:55:26 +0900

    session

--- init.el
+++ init.el
@@ -35,7 +35,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(session use-package)))
+ '(package-selected-packages '(session use-package))
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
