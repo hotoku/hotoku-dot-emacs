f5226a4e768d1b3113a1ec3a737c350c1de2db39
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-01-26 17:18:22 +0900

    use-package, magit

---
+++ init.el
@@ -0,0 +1,40 @@
+;; -*- lexical-binding: t -*-
+
+
+
+;;; package
+(require 'package)
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+(package-refresh-contents)
+
+
+;;; use-package initialize
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;;; magit
+(use-package magit)
+
+;;; misc
+(show-paren-mode)
+
+
+;;; custom
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
