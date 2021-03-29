1dd78fb04f9edf938fe3382fc2f68a148dc8dc03
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-03-29 20:55:14 +0900

    pyenv-mode-auto

--- init.el
+++ init.el
@@ -351,6 +351,8 @@
   :hook
   ((js-mode . prettier-js-mode)))
 
+(use-package pyenv-mode-auto)
+
 
 ;;; misc
 ;; make backup files in a specific directory
@@ -414,7 +416,7 @@ This is inconvinient when opening file at the beginning of Emacs session."
  '(custom-safe-themes
    '("246cd0eb818bfd347b20fb6365c228fddf24ab7164752afe5e6878cb29b0204e" default))
  '(package-selected-packages
-   '(prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company session use-package))
+   '(pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company session use-package))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
