1b3a9529f8abbae7fb897c8ce260654c9718c2da
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-05-18 17:06:18 +0900

    yh-sql

--- yh/yh-sql.el
+++ yh/yh-sql.el
@@ -10,9 +10,28 @@
   "Format buffer with zetasql-formatter.
 https://github.com/Matts966/zetasql-formatter."
   (interactive)
-  (let* ((fpath (buffer-file-name)))
-    (call-process-shell-command (format "zetasql-formatter %s" fpath))
-    (revert-buffer t t)))
+  (let* ((curbuf (current-buffer))
+         (curpnt (point))
+         (outbuf-name "*zetasql*")
+         (outbuf (get-buffer outbuf-name))
+         (fpath (buffer-file-name)))
+    (when outbuf
+      (save-excursion
+        (switch-to-buffer outbuf)
+        (erase-buffer)))
+    (let ((ret (call-process-shell-command (format "zetasql-formatter %s" fpath)
+                                           nil outbuf-name))
+          ;; DIRTY HACK.
+          ;; zetasql-formatter return non-zero value when the formatted string is
+          ;; different from the original even if it was syntactically correct.
+          ;; Here, we want to change to *zetasql* buffern only when it is syntactically
+          ;; incorrect. We format the file twice and check the output of second run.
+          (ret2 (call-process-shell-command (format "zetasql-formatter %s" fpath)
+                                            nil outbuf-name)))
+      (if (= ret2 0)
+          (progn (switch-to-buffer curbuf)
+                 (revert-buffer t t)
+                 (goto-char curpnt))))))
 
 (provide 'yh-sql)
 ;;; yh-sql.el ends here
