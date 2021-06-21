9ed0806b90b6b652f6c63ec2e783aaf114441b81
Yasunori Horikoshi <yasunori.horikoshi@jdsc.ai>
2021-06-21 11:54:29 +0900

    remove pyvenv, introduce gitignore-mode:
    I decided to always launch emacs from shell and pyvenv is not
    necessary now.

--- init.el
+++ init.el
@@ -360,12 +360,12 @@
 
 (use-package bazel)
 
-(use-package pyvenv)
-
 (use-package helm-ag
   :defer t
   :init
   (setq helm-ag-base-command "rg"))
+
+(use-package gitignore-mode)
 
 
 ;;; misc
@@ -430,7 +430,7 @@ This is inconvinient when opening file at the beginning of Emacs session."
  '(custom-safe-themes
    '("57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" "246cd0eb818bfd347b20fb6365c228fddf24ab7164752afe5e6878cb29b0204e" default))
  '(package-selected-packages
-   '(helm-ag pyenv  afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company session use-package))
+   '(gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company session use-package))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
