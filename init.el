5cb52186bc35d9eb1559de04e02db141d490310d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-07-07 20:12:18 +0900

    wip

--- init.el
+++ init.el
@@ -29,8 +29,8 @@
 
 ;;; configuration of packages
 (use-package yh :ensure nil
-  :config
-  (yh/package-refresh-contents))
+             :config
+             (yh/package-refresh-contents))
 
 (use-package gnu-elpa-keyring-update) ; This should be first.
 
@@ -216,12 +216,12 @@
             #'(lambda () (add-hook 'before-save-hook 'yh-docker-upcase-command nil t))))
 
 (use-package yh-sql :ensure nil
-  :commands yh-sql-format
-  :hook (sql-mode . (lambda () (add-hook 'after-save-hook 'yh-sql-format nil t))))
+             :commands yh-sql-format
+             :hook (sql-mode . (lambda () (add-hook 'after-save-hook 'yh-sql-format nil t))))
 
 (use-package yh-font :ensure nil
-  :config
-  (yh-font-initialize))
+             :config
+             (yh-font-initialize))
 
 (use-package elisp-mode
   :ensure nil
@@ -461,7 +461,7 @@ This is inconvinient when opening file at the beginning of Emacs session."
  '(custom-safe-themes
    '("57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" "246cd0eb818bfd347b20fb6365c228fddf24ab7164752afe5e6878cb29b0204e" default))
  '(package-selected-packages
-   '(lsp-docker poetry gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company session use-package))
+   '(il lsp-docker poetry gitignore-mode helm-ag pyenv afuternoon-theme afternoon-theme bazel-mode pyenv-mode-auto prettier-js dap-python py-autopep8 flymake-yaml dockerfile-mode biblio elpy haskell-mode yaml-mode json-mode gnu-elpa-keyring-update undo-tree git-ps1-mode ace-window flycheck yasnippet open-junk-file dakrone-theme smartparens helm company session use-package))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
